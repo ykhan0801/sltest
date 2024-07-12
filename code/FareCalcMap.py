13798e155069d7f397a72e3e4775b115a780bd96
ykhan0801 <108740407+ykhan0801@users.noreply.github.com>
2024-07-12 13:46:27 +0100

    Update FareCalcMap.py

--- code/FareCalcMap.py
+++ code/FareCalcMap.py
@@ -9,6 +9,7 @@ import streamlit as st
 from streamlit_folium import st_folium
 from pathlib import Path
 from shapely.geometry import Polygon
+from shapely.geometry import mapping
 from folium.plugins import BeautifyIcon
 from branca.element import Template, MacroElement
 import streamlit.components.v1 as components
@@ -16,22 +17,19 @@ import streamlit.components.v1 as components
 # st.set_page_config(layout="centered")
 st.set_page_config(layout="wide")
 
-# Origin station and ticket selection
-#chosen_station = "Greystones"
-#chosen_ticket_type = "Child Single"
-#chosen_payment_type = "Leap Fares"
-
 # Map settings
 map_centre = (53.33985783249015, -6.273211120984975)
 zoom = 9
 
-#main_path = r"C:\Users\ykhan1\DevOps\DataScienceTeam\Projects\NTA Fare Calculations"
 main_path = os.path.dirname(__file__)
 data_path = os.path.join(main_path, "data")
 gis_path = os.path.join(main_path, "GIS") 
 
+logo_image_path = os.path.join(main_path,'..', 'pics', "logo2.png")
+st.sidebar.image(logo_image_path, width = 300)
+
 # Read in data for OD pairs and fares, filter for the ticket selection
-fare_zone_data = pd.read_csv(os.path.join(main_path, '..', 'data', "OD_Pairs.csv"), encoding='unicode_escape')
+fare_zone_data = pd.read_csv(os.path.join(main_path, '..', 'data', "OD_Pairs2.csv"), encoding='unicode_escape')
 fares_df = pd.read_csv(os.path.join(main_path,'..', 'data',  "Fares.csv"), encoding='unicode_escape')
 
 payment_list = list(fares_df["PaymentMeans"].unique())
@@ -56,8 +54,9 @@ fares_from_chosen_station = fare_zone_data.loc[fare_zone_data["Origin"]==chosen_
 fares_from_chosen_station = fares_from_chosen_station.merge(rail_nodes, left_on="Destination", right_on="stop_name")
 fares_from_chosen_station = fares_from_chosen_station.merge(fares_df, left_on="Value", right_on="FareZone")
 
+unique_fares = fares_df.drop_duplicates(subset=['Fare'])
 fare_num = len(fares_df)
-cm  = plt.get_cmap('Blues')
+cm  = plt.get_cmap('viridis_r')
 indices = np.linspace(0, 1, fare_num)
 
 colour_dict = {}
@@ -67,6 +66,31 @@ for i, index in enumerate(indices):
     k = fares_df.iloc[i]['FareZone']
     colour_dict[f'{k}'] = rgb
 
+keys = list(colour_dict.keys())
+for i in range(1, len(keys)):
+    key = keys[i]
+    prev_key = keys[i-1]
+    if fares_df.iloc[i]['Fare'] == fares_df.iloc[i-1]['Fare']:
+        colour_dict[key] = colour_dict[prev_key]
+
+colour_dict2 = {
+    "D1": (252, 137, 5),
+    "D2": (245, 225, 2),
+    "D10": (136, 245, 2),
+    "D14": (3, 166, 6),
+    "D11": (2, 242, 238),
+    "D90": (2, 242, 238),
+    "D12": (2, 110, 242),
+    "D91": (2, 110, 242),
+    "D13": (1, 31, 138),
+    "D92": (1, 31, 138),
+    "D93": (112, 4, 201),
+    "D94": (247, 2, 231),
+    "D95": (184, 4, 79),
+}
+
+colour_dict2 = {k: tuple(v/255 for v in rgb) for k, rgb in colour_dict2.items()}
+
 # Create map
 m = folium.Map(
     location=map_centre,
@@ -75,19 +99,30 @@ m = folium.Map(
     tiles="CartoDB positron",
 )
 
+#@st.cache_data
+#def load_rail(main_path):
+#    rail_df = gpd.read_file(os.path.join(main_path, '..', 'GIS', "routesint.shp"))
+#    return rail_df
+
+#Rail Lines
+#rail_df = load_rail(main_path)
+#folium.GeoJson(rail_df, name='multi_line', style_function=lambda x:{
+#    'color': '#9E9E9E',
+#    'weight': 1}).add_to(m)
+
 #Polygon shapes for city and commuter zones.
 folium.GeoJson(city_zone, style_function=lambda x:{
-    'fillColor' : 'blue',
+    'fillColor' : '#db078d',
     'color' : '#db078d',
     'weight': 2,
-    'fillOpacity': 0
+    'fillOpacity': 0.05
 }).add_to(m)
 
 folium.GeoJson(commuter_zone, style_function=lambda x:{
-    'fillColor' : 'red',
+    'fillColor' : '#5b0896',
     'color' : '#5b0896',
     'weight': 2,
-    'fillOpacity': 0
+    'fillOpacity': 0.05
 }).add_to(m)
 
 destination_list = sorted(list(fare_zone_data["Destination"].unique()))
@@ -96,6 +131,15 @@ destination_station = st.sidebar.selectbox("Select a Destination Station:", dest
 
 if destination_station == chosen_station:
     st.sidebar.write("WARNING! The selected origin and destination stations are the same.")
+
+if destination_station != chosen_station and destination_station !="Any":
+    st.sidebar.markdown('### You Have Selected:')
+    st.sidebar.write(f" A {chosen_payment_type[:-1]} - {chosen_ticket_type} Ticket from {chosen_station} to {destination_station}.")
+    for i, row in fares_from_chosen_station.iterrows():
+        if row['Origin'] == chosen_station and row['Destination'] == destination_station:
+            fare_type = fares_from_chosen_station.iloc[i]['Value']
+            fare_cost = fares_from_chosen_station.iloc[i]['Fare']
+            st.sidebar.write(f"This journey is in Zone {fare_type} and will cost €{fare_cost:.2f}.")
 
 if destination_station == "Any":
     #Loop through unique stations to add marker on map
@@ -109,11 +153,11 @@ if destination_station == "Any":
 
         folium.CircleMarker(
                         location=coords,
-                        radius=5,
+                        radius=6,
                         color="#4e4e4e",
                         opacity=0.6,
                         weight=1,
-                        fill_color=colors.rgb2hex(colour_dict[fare_zone], keep_alpha=True),
+                        fill_color=colors.rgb2hex(colour_dict2[fare_zone], keep_alpha=True),
                         fill_opacity=1,
                         tooltip=tool_tip,
                     ).add_to(m)
@@ -128,7 +172,7 @@ elif destination_station != chosen_station:
 
     folium.CircleMarker(
                     location=coords,
-                    radius=5,
+                    radius=6,
                     color="#4e4e4e",
                     opacity=0.6,
                     weight=1,
@@ -136,8 +180,7 @@ elif destination_station != chosen_station:
                     fill_opacity=1,
                     tooltip=tool_tip,
                 ).add_to(m)
-
-    
+   
 # Star marker for origin station
 icon_star = BeautifyIcon(
 icon='diamond',
@@ -150,7 +193,7 @@ origin_coords = [rail_nodes.loc[rail_nodes['stop_name'] == chosen_station, 'stop
                 rail_nodes.loc[rail_nodes['stop_name'] == chosen_station, 'stop_lon'].iloc[0]]
 folium.Marker(location=origin_coords, tooltip=f'Origin Station: {chosen_station}', icon=icon_star).add_to(m)
 
-colour_map_dict = {key: tuple(int(255* value)for value in rgb) for key, rgb in colour_dict.items()}
+colour_map_dict = {key: tuple(int(255* value)for value in rgb) for key, rgb in colour_dict2.items()}
 
 # Horizontal Legend
 fare_zone_html_string = ""
@@ -167,8 +210,10 @@ legend_table_html = '''
 <!doctype html>
 <html lang="en">
 <head><style>
+@import url('https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 table{
 border: 1px solid black;
+font-family: "Prompt";
 }
 th, td {
 padding:8px;windows move 
@@ -184,15 +229,17 @@ text-align: center;
 legend_html_end = "</tr><tr>" + fare_price_html_string + "</tr><tr>" + colour_html_string + "</tr></tbody></table></body></html>"
 legend_table_html += legend_html_end
 
-m.save(os.path.join(main_path, '..', "outputs\output.html"))
-
+#m.save(os.path.join(main_path, '..', "outputs\output.html"))
 
 legend_vertical_html = '''
 <!doctype html>
 <html lang="en">
 <head><style>
+@import url('https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 table{
 border: 1px solid black;
+width: 100%;
+font-family: "Prompt";
 }
 th, td {
 padding:8px;windows move 
@@ -203,7 +250,7 @@ text-align: center;
     <table cellspacing="0">
         <tbody>
         <tr>   
-        <td style= "background-color: rgb(219, 7, 141);"</td><td> - City Zone Boundary</td>
+        <td style= "background-color: rgb(219, 7, 141); "</td><td> - City Zone Boundary</td>
         </tr>
         <tr> 
         <td style= "background-color: rgb(91, 8, 150);"></td><td> - Commuter Zone Boundary</td>  
@@ -217,7 +264,7 @@ text-align: center;
 </html>
 '''
 
-map_col, legend_col = st.columns([0.8,0.2])
+map_col, legend_col = st.columns([0.85,0.15])
 with map_col:
     #st.header("NTA Fare Calculations Map")
     components.html(legend_table_html,
@@ -227,4 +274,16 @@ with map_col:
 with legend_col:
     st.header("")
     st.header("")
-    components.html(legend_vertical_html, height=300)
+    legend_image_path = os.path.join(main_path,'..', 'pics', "legend2.png")
+    st.image(legend_image_path, width = 250)
+    #components.html(legend_vertical_html, height=300)
+    for i in range(6):
+        st.header("")
+    st.text("")
+    systra_image_path = os.path.join(main_path,'..', 'pics', "Systra.png")
+    st.image(systra_image_path, width = 120)
+
+st.write("Map Data", st_data)
+if st_data and st_data.get("last_object_clicked"):
+    last_clicked = st_data["last_object_clicked"]
+    st.write(f"Clicked Location: {last_clicked}")
